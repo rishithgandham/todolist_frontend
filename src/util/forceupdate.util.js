67532b2768dae78e68e74bda5b4cc665ea32231e
RishithGandham <rishith.gandham@gmail.com>
2022-06-05 10:22:06 -0400

    force update for delete and edit modals

---
+++ src/util/forceupdate.util.js
@@ -0,0 +1,12 @@
+// import React, {useState} from 'react'
+
+
+// function useForceUpdate() {
+//     const [dummyState, setDummyState] = useState(0);
+//     return () => setDummyState(dummyState + 1);
+// }
+
+// export default useForceUpdate;
+
+
+/* code does not work */
