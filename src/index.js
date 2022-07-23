8ef072653d1a2ab3880016aee449f92fa2af03a9
RishithGandham <rishith.gandham@gmail.com>
2022-07-23 12:23:57 -0400

    changed names for routes

--- src/index.js
+++ src/index.js
@@ -14,15 +14,16 @@ import NavbarComponent from './component/navbar/navbar.component'
 
 
 //pages
-import Landing from './page/landing/landing.component'
-import Login from './page/login/login.component'
-import ProtectedRoute from './auth/protectedroute.component';
+import Landing from './page/landing/landing.page'
+import LoginPage from './page/login/login.page'
 import HomePage from './page/home/home.page';
-import SignupComponent from './page/signup/signup.component';
-import EmailSentComponent from './page/emailsent/emailsent.component';
-import ConfirmPageComponent from './page/emailsent/confirmpage.component';
+import SignUpPage from './page/signup/signup.page';
+import EmailSentComponent from './page/emailsent/emailsent.page';
+import ConfirmPageComponent from './page/emailsent/confirmpage.page';
 import ViewListPage from './page/viewlist/viewlist.page';
 import ViewlistsPage from './page/viewlists/viewlists.page';
+
+import ProtectedRoute from './auth/protectedroute.component';
 
 import {ToastContainer} from 'react-toastify';
 
@@ -57,11 +58,12 @@ const Router = () => {
                 <NavbarComponent authState={navState} />
                 <Routes>
                     <Route path='/' element={<Landing />}></Route>
-                    <Route path='/login' element={<Login setNav={setNavState} />}></Route>
-                    <Route path='/register' element={<SignupComponent setNav={setNavState} />}></Route>
+                    <Route path='/login' element={<LoginPage setNav={setNavState} />}></Route>
+                    <Route path='/register' element={<SignUpPage setNav={setNavState} />}></Route>
                     <Route path='/emailsent/:email' element={<EmailSentComponent />}></Route>
-                    <Route path='/home' element={<ProtectedRoute setNav={setNavState}><HomePage /></ProtectedRoute>}></Route>
                     <Route path='/confirm/:token' element={<ConfirmPageComponent />}></Route>
+                    {/* paths requiring authentication */}
+                    <Route path='/home' element={<ProtectedRoute setNav={setNavState}><HomePage /></ProtectedRoute>}></Route>
                     <Route path='/viewlists' element={<ProtectedRoute setNav={setNavState}><ViewlistsPage /></ProtectedRoute>}></Route>
                     <Route path='/viewlist' element={<ProtectedRoute setNav={setNavState}><ViewListPage /></ProtectedRoute>}></Route>
                 </Routes>
---
+++ src/page/profile/profile.page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ProfilePage(props) {
+    
+    
+
+    return (
+        <>
+            <div className='text-center mt-5'>
+                <h1 className='display-4'>Profile</h1>
+            </div>
+            
+        </>
+    )
+}
